df96913e691dd3d7db1bbf1bb67f286ba8c36570
mathieu-LAGNY <mathieu.lagny@etu.umontpellier.fr>
2020-01-07 11:58:31 +0100

    correction comment

--- src/main/java/fr/umontpellier/iut/conquest/Board.java
+++ src/main/java/fr/umontpellier/iut/conquest/Board.java
@@ -156,7 +156,7 @@ public class Board {
     }
 
     /**
-     * Retourne la liste de tous les pions d'un joueur.
+     * Retourne le nombre de pions d'un joueur.
      */
     public int getNbPawns(Player player) {
         int nbPawns = 0;
