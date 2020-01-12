7a136c857e252dc53e91d09177397e90501196ce
amaury-jaspar <amaury.jaspar@etu.umontpellier.fr>
2020-01-12 17:43:52 +0100

    minor correction in NaiveTest

--- src/test/java/fr/umontpellier/iut/conquest/NaiveTest.java
+++ src/test/java/fr/umontpellier/iut/conquest/NaiveTest.java
@@ -2,28 +2,26 @@ package fr.umontpellier.iut.conquest;
 
 import fr.umontpellier.iut.conquest.strategies.Naive;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class NaiveTest {
 
-    private Board b;
+    private Board board;
     private Player player1 = new Player(null, null, null, 1);
     private Naive naive = new Naive();
     private Player aiPlayer = new Player(naive, null, null, 2);
 
     @BeforeEach
     void create_board() {
-        b = new Board(5);
-        b.initField(player1, aiPlayer);
+        board = new Board(5);
+        board.initField(player1, aiPlayer);
     }
 
     @Test
-    void move_should_be_in_valid_moves_list() {
-        Move move = naive.getMove(b, aiPlayer);
-        assertTrue(b.getValidMoves(aiPlayer).contains(move));
+    void move_should_be_valid() {
+        Move move = naive.getMove(board, aiPlayer);
+        assertTrue(board.isValid(move, aiPlayer));
     }
-
 }
