ebcdd95e39135f448a4e61b0011f7fe91d725853
amaury-jaspar <amaury.jaspar@etu.umontpellier.fr>
2020-01-13 18:33:16 +0100

    addition of a MinMaxPerfTest class to test the MinMax algorithm's performance

---
+++ src/test/java/fr/umontpellier/iut/conquest/MinMaxPerfTest.java
@@ -0,0 +1,24 @@
+package fr.umontpellier.iut.conquest;
+
+import fr.umontpellier.iut.conquest.strategies.Minmax;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MinMaxPerfTest {
+
+    @Test
+    void run_time_for_2_AIs_of_level_4_on_board_of_size_5_should_be_less_than_30s() {
+
+        Minmax ai1 = new Minmax(4);
+        Minmax ai2 = new Minmax(4);
+        Game game = new Game(5, ai1, null, ai2, null);
+
+        long startTime = System.nanoTime();
+        game.run(1);
+        long endTime = System.nanoTime() - startTime;
+
+        System.out.println(endTime);
+        assertTrue(endTime < 30000000000.); //30 000 000 000 nano seconds = 30s
+    }
+}
